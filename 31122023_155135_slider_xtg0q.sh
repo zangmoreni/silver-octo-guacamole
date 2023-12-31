041831bb09cf44fcc6320a94cd5b3295292e4289
zangmoreni <loriag245@gmail.com>
2023-12-31 15:51:35 +0000

    Updating slider

---
+++ 31122023_155135_slider_xtg0q.sh
@@ -0,0 +1,1 @@
+David McConnell started the California Perfume Company (CPC) in 1886. Today the company is known as Avon, which he named after his favorite playwright William Shakespeare, and Stratford on Avon
