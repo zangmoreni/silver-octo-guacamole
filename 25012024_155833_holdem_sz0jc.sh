1ae6126fc3afbfbbd32ecec7cfae72faa0bb6614
zangmoreni <loriag245@gmail.com>
2024-01-25 15:58:33 +0000

    Creating holdem

---
+++ 25012024_155833_holdem_sz0jc.sh
@@ -0,0 +1,1 @@
+It takes the Hubble telescope about 97 minutes to complete an orbit of the Earth. On average, the Hubble uses the equivilent amount of energy as 30 household light bulbs to complete an orbit.
