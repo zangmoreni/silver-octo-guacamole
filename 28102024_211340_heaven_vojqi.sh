fc938b59aef7f775810ffe14615af6136bef7339
zangmoreni <loriag245@gmail.com>
2024-10-28 21:13:40 +0000

    Fixing heaven

---
+++ 28102024_211340_heaven_vojqi.sh
@@ -0,0 +1,1 @@
+Parma ham is only Parma ham if it is made in the Parma region of Italy. The British chain supermarket Asda, made and packaged its own "Parma ham" and was successfully sued by the real Parma ham people (Parma Ham Trade Association)
